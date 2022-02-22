d37ba67566bfa6c179067a009498d2a96113bb93
Constantine Kurianoff <kurianoff@gmail.com>
2022-02-21 23:17:11 -0500

    binary media types

--- api/main.tf
+++ api/main.tf
@@ -63,7 +63,8 @@ resource "aws_api_gateway_rest_api" "api" {
     "image/jpeg",
     "image/png",
     "image/gif",
-    "application/pdf"
+    "application/pdf",
+    "application/octet-stream"
   ]
 
   minimum_compression_size     = -1
